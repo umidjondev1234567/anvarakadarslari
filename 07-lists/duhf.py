f54d0442a10d07dd76e52ec0872b4635c33c1a60
umidjondev1234567 <103695341+umidjondev1234567@users.noreply.github.com>
2022-10-09 17:39:13 +0500

    bbb

---
+++ 07-lists/duhf.py
@@ -0,0 +1,9 @@
+# # -*- coding: utf-8 -*-
+# """
+# Created on Sun Oct  9 13:12:17 2022
+
+# @author: User
+# """
+
+# print("GitHub desktop uchun")
+print()
--- duhf.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct  9 13:12:17 2022
-
-@author: User
-"""
-
-print("GitHub desktop uchun")
